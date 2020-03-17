14df86b317bcdb46d7516827e969952ac7312f07
Xiaochen Liu <xcliu@fb.com>
2020-03-16 17:34:43 -0700

    update setup

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@ sudo apt-get install python-tk
 
 pip install -r requirements.txt
 
-pip install git+https://github.com/ucbrise/clipper.git@develop#subdirectory=clipper_admin
-
 git clone https://github.com/thtrieu/darkflow.git
 
 cd darkflow 
